65d3befa9ebe40036629150d9f45738d93be4e1a
WINDOWS-S2UHN56\user <tongysmember@hotmail.com>
2022-09-30 10:30:01 +0800

    [Update] Update Class Attribute

--- SA.py
+++ SA.py
@@ -10,40 +10,41 @@ class SA(object):
         self.func_objective = func_objective
         self.list_point = list()
         print("Init_SA")
+        self.Temprate_now = 1000
+        self.Temprate_start = self.Temprate_now
+        self.Temprate_min = 0.3
+        self.time = 0
+        self.k = 50                                     
+        self.upper_bound = 500
+        self.lower_bound = -500
 
     def proceed(self):
-        # 模拟退火模型构建
-        T  = 1000                                # 温度
-        T0 = T                                      # 初始温度
-        Tmin = 0                                    # 最小温度
-        Tmin = 0.5                                  # 最小温度
-        t = 0                                       # 时间
-        k = 50                                      # 每个温度的迭代次数
-        upper_bound = 500
-        lower_bound = -500
-        x1 = np.random.uniform(low=lower_bound,high=upper_bound)       # 随机化初始值
-        x2 = np.random.uniform(low=lower_bound,high=upper_bound)       # 随机化初始值
+        '''
+        Process
+        '''
+        x1 = np.random.uniform(low=self.lower_bound,high=self.upper_bound)       # 随机化初始值
+        x2 = np.random.uniform(low=self.lower_bound,high=self.upper_bound)       # 随机化初始值
         self.list_point.append([x1, x2])
         y = 0                                       # y初始值
         step = 5
 
-        while T > Tmin:
-            for i in range(k):
+        while self.Temprate_now > self.Temprate_min:
+            for _ in range(self.k):
                 y = self.func_objective(x1,x2)                                  # 目标函数
                 x1New = x1 + np.random.uniform(low=-1*step,high=+1*step)   # 更新xNew
                 x2New = x2 + np.random.uniform(low=-1*step,high=+1*step)   # 更新xNew
                 yNew = self.func_objective(x1New, x2New)							# 求出新状态值
-                if (x1New < upper_bound and x1New > lower_bound) and (x2New < upper_bound and x2New > lower_bound):							# 状态比较
+                if (x1New < self.upper_bound and x1New > self.lower_bound) and (x2New < self.upper_bound and x2New > self.lower_bound):							# 状态比较
                     if y > yNew:
                         x1 = x1New
                         x2 = x2New
                     else:
                         r = np.random.uniform(low=0,high=1)         # 随机数
-                        p = math.exp(-(yNew-y)/T)                   # 概率值
+                        p = math.exp(-(yNew-y)/self.Temprate_now)                   # 概率值
                         if r < p:                                   # 小于则更新
                             x1 = x1New
                             x2 = x2New
-            t = t + 1
-            T = T0/(1+t)                                            # 快速模拟退火算法降温方式
-            print("t:",t," T:",T,'x1:{0}, x2:{1}'.format(x1,x2))
+            self.time = self.time + 1
+            self.Temprate_now = self.Temprate_start/(1+self.time)                                            # 快速模拟退火算法降温方式
+            print("Times:",self.time," Temprate_now:",self.Temprate_now,'x1:{0}, x2:{1}'.format(x1,x2))
             self.list_point.append([x1, x2])
