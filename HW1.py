f477bdf18932087e735831d03175fa39cb6a9d28
Mike Tung <tongysmember@gmail.com>
2022-10-01 00:14:26 +0800

    [Update] Main Code Block

--- HW1.py
+++ HW1.py
@@ -1,23 +1,39 @@
-from Objective import Objective
-from SA import SA
-from GA4 import GeneAlgo
+from FUNC.Objective import Objective
+from ALGO.SA import SA
+from ALGO.GA4 import GeneAlgo
 
-from GRAPH import Objective_Graph
+from FUNC.GRAPH import Objective_Graph
 
 
 def main():
 
     func_objctive = Objective.func_objective_q2
-    
-    SA_Obj = SA(func_objctive)
-    SA_Obj.proceed()
-    
-    #GA = GeneAlgo()
-    #GA.proceed()
+    lower_bound, upper_bound = -500, 500
+    sa_update_step =5
+    graph = Objective_Graph(upper_bound,.1)
+    repeat_value_range = 10
 
-    graph = Objective_Graph(500,.1)
-    #graph.Draw_graph(Objective.func_objective_q2, list(SA_Obj.list_point)[::int(len(SA_Obj.list_point)/1000)])
-    graph.Draw_graph(Objective.func_objective_q2, list(SA_Obj.list_point))
+    #func_objctive = Objective.func_objective_q5
+    #lower_bound, upper_bound = -10, 10
+    #sa_update_step =.1
+    #graph = Objective_Graph(upper_bound,1)
+    #repeat_value_range = 1
+
+    #iteration_times = 10
+    #SA_Obj = SA(lower_bound, upper_bound, iteration_times, func_objctive, sa_update_step, repeat_value_range)
+    #SA_Obj.proceed()
+    #graph.Draw_graph_2D(func_objctive, list(SA_Obj.list_point))
+    #graph.Draw_graph_3D(func_objctive, list(SA_Obj.list_point))
+
+    
+    iteration_times = 5000
+    GA = GeneAlgo(lower_bound, upper_bound, iteration_times, func_objctive)
+    GA.proceed()
+    graph.Draw_graph_2D(func_objctive, list(GA.list_point))
+    graph.Draw_graph_3D(func_objctive, list(GA.list_point))
+  
+    
+    
 
 if __name__ == '__main__':
     main()
