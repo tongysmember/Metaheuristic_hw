144c6dedeaf3b1ae273584cf1369c765fc835b5d
Mike Tung <tongysmember@gmail.com>
2022-10-01 02:00:15 +0800

    [Update] Main Comment

--- HW1.py
+++ HW1.py
@@ -1,12 +1,18 @@
 from FUNC.Objective import Objective
 from ALGO.SA import SA
-from ALGO.GA4 import GeneAlgo
+from ALGO.GA import GA
 
 from FUNC.GRAPH import Objective_Graph
 
 
 def main():
+    '''
+    1. 設定初始變數
+    2. 計算 Simulate Annealing Algorighm
+    3. 計算 Genetic Algorithm
+    '''
 
+    # 設定初始變數, 目標函式, 上下界資訊, 迭代次數
     func_objctive = Objective.func_objective_q2
     lower_bound, upper_bound = -500, 500
     sa_update_step =5
@@ -19,18 +25,20 @@ def main():
     #graph = Objective_Graph(upper_bound,1)
     #repeat_value_range = 1
 
-    #iteration_times = 10
-    #SA_Obj = SA(lower_bound, upper_bound, iteration_times, func_objctive, sa_update_step, repeat_value_range)
-    #SA_Obj.proceed()
-    #graph.Draw_graph_2D(func_objctive, list(SA_Obj.list_point))
-    #graph.Draw_graph_3D(func_objctive, list(SA_Obj.list_point))
-
     
-    iteration_times = 5000
-    GA = GeneAlgo(lower_bound, upper_bound, iteration_times, func_objctive)
-    GA.proceed()
-    graph.Draw_graph_2D(func_objctive, list(GA.list_point))
-    graph.Draw_graph_3D(func_objctive, list(GA.list_point))
+    # Simulate Annealing Algorighm
+    iteration_times = 10
+    SA_Obj = SA(lower_bound, upper_bound, iteration_times, func_objctive, sa_update_step, repeat_value_range)
+    SA_Obj.proceed()
+    #graph.Draw_graph_2D(func_objctive, list(SA_Obj.list_point))
+    graph.Draw_graph_3D(func_objctive, list(SA_Obj.list_point))
+
+    ## Genetic Algorithm
+    #iteration_times = 5000
+    #GA_Obj = GA(lower_bound, upper_bound, iteration_times, func_objctive)
+    #GA_Obj.proceed()
+    #graph.Draw_graph_2D(func_objctive, list(GA_Obj.list_point))
+    #graph.Draw_graph_3D(func_objctive, list(GA_Obj.list_point))
   
     
     
