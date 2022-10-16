02de122620533ffef317ec1eb30d000daa1b6fcc
Mike Tung <tongysmember@gmail.com>
2022-10-16 19:51:53 +0800

    [Add] HW2 Question_Q11

--- FUNC/Objective.py
+++ FUNC/Objective.py
@@ -25,7 +25,21 @@ class Objective(object):
 
     # Homework 2 ===================================================
     @staticmethod
-    def func_objective_hw2_q2(x, D:int=10):
+    def func_objective_hw2_q2_D10(x, D:int=10):
+        sum_value = 0
+        for i in range(1,D,1):
+            sum_value += x**4
+        return sum_value
+
+    @staticmethod
+    def func_objective_hw2_q2_D20(x, D:int=20):
+        sum_value = 0
+        for i in range(1,D,1):
+            sum_value += x**4
+        return sum_value
+    
+    @staticmethod
+    def func_objective_hw2_q2_D50(x, D:int=50):
         sum_value = 0
         for i in range(1,D,1):
             sum_value += x**4
@@ -36,4 +50,23 @@ class Objective(object):
         sum_value = 0
         for i in range(1,D,1):
             sum_value += i*(x**2)
+        return sum_value
+    
+    @staticmethod
+    def func_objective_hw2_q6(x, D:int=10):
+        sum_value,sum_value_1,sum_value_2 = 0, 0, 0
+        for i in range(1,D,1):
+            sum_value_1 += (x**2)
+        for i in range(2,D,1):
+            sum_value_2 += (x**2)
+        sum_value_2 = (10**6)*sum_value_2
+        return sum_value_1+sum_value_2
+
+    @staticmethod
+    def func_objective_hw2_q11(x, D:int=10):
+        sum_value = 0
+        D = D-2
+        for i in range(1,D,1):
+            sum_value += (x + 10*x)**2 + 5*((x-x)**2) + (x-2*x)**4 + 10*((x-x)**4)
+        
         return sum_value
